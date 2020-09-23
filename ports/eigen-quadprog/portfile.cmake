28b13ac47e3e9078523c4ae8ff9e7f3a16a30f51
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-09-23 21:54:25 +0800

    [eigen-quadprog] Add new port

---
+++ ports/eigen-quadprog/portfile.cmake
@@ -0,0 +1,37 @@
+include(vcpkg_find_fortran_for_subdirectory)
+
+vcpkg_find_fortran_for_subdirectory(FORTRAN_CMAKE)
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+    set(MINGW_PATH mingw32)
+else()
+    set(MINGW_PATH mingw64)
+endif()
+set(MINGW_BIN "${vcpkg_find_fortran_for_subdirectory_MSYS_ROOT}/${MINGW_PATH}/bin")
+set(MINGW_GFORTRAN "${MINGW_BIN}/gfortran.exe")
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/jrl-umi3218/eigen-quadprog/releases/download/v1.0.1/eigen-quadprog-v1.0.1.tar.gz"
+    FILENAME "eigen-quadprog-v1.0.1.tar.gz"
+    SHA512 c4026ff1c4c3213d43e81a46d71f22073355642486144dc0bd73528bd61d66aab14d414ea5a67580e7bd47fd9eb60ef669fcda180ac53c951e05a778501bbb7c
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    REF 1.0.1
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+      -DBUILD_TESTING=OFF
+      -DPYTHON_BINDING=OFF
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+
+file(INSTALL ${SOURCE_PATH}/COPYING.LESSER DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
