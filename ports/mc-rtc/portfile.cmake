50ce3247d8abf07424d8b5f372f123360926f956
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-09-24 10:48:32 +0800

    [mc-rtc] Correctly handle Python tools

--- ports/mc-rtc/portfile.cmake
+++ ports/mc-rtc/portfile.cmake
@@ -27,17 +27,37 @@ file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
+set(MC_RTC_TOOLS
+  mc_bin_perf
+  mc_bin_to_flat
+  mc_bin_to_log
+  mc_bin_utils
+  mc_json_to_yaml
+  mc_old_bin_to_flat
+)
+
+set(MC_RTC_PYTHON_TOOLS
+  mc_log_ui
+  mc_plot_logs
+  mc_rtc_new_controller
+  mc_rtc_new_fsm_controller
+)
+
+if(WIN32)
+  foreach(tool_name ${MC_RTC_PYTHON_TOOLS})
+    set(tool_path "${CURRENT_PACKAGES_DIR}/bin/${tool_name}.py")
+    if(EXISTS ${tool_path})
+      file(COPY ${tool_path} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+    endif()
+  endforeach()
+else()
+  list(APPEND MC_RTC_TOOLS ${MC_RTC_PYTHON_TOOLS})
+endif()
+
 vcpkg_copy_tools(
   TOOL_NAMES
-    mc_bin_perf
-    mc_bin_to_flat
-    mc_bin_to_log
-    mc_bin_utils
-    mc_json_to_yaml
-    mc_log_ui
-    mc_old_bin_to_flat
-    mc_plot_logs
-    mc_rtc_new_controller
-    mc_rtc_new_fsm_controller
+    ${MC_RTC_TOOLS}
   AUTO_CLEAN
 )
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
