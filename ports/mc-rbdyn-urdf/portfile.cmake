928421d525aedbcbbbfb8cb92321565bee8a40ca
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-09-24 08:05:21 +0800

    [mc-rbdyn-urdf] Correct name of exported package

--- ports/mc-rbdyn-urdf/portfile.cmake
+++ ports/mc-rbdyn-urdf/portfile.cmake
@@ -20,7 +20,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/mc_rbdyn_urdf)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/mc_rbdyn_urdf TARGET_PATH share/mc_rbdyn_urdf)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
