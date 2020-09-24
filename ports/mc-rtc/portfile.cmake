d9b092f3a4912b00fe90babfd1f0c5d891b07349
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-09-24 08:05:37 +0800

    [mc-rtc] Correct name of exported package

--- ports/mc-rtc/portfile.cmake
+++ ports/mc-rtc/portfile.cmake
@@ -21,7 +21,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/mc_rtc)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/mc_rtc TARGET_PATH share/mc_rtc)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
