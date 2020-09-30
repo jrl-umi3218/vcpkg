a8386124438eb655674ef4207f95caa5dfd814f0
Pierre Gergondet <pierre.gergondet@gmail.com>
2020-09-30 08:37:21 +0800

    [python] Use pip3/python3 as cmake will

--- scripts/cmake/vcpkg_amend_python_path.cmake
+++ scripts/cmake/vcpkg_amend_python_path.cmake
@@ -1,6 +1,6 @@
 ## # vcpkg_amend_python_path
 ##
-## Add the ${CURRENT_PACKAGES_DIR}/lib/python3.X/site-packages to PYTHONPATH
+## Add the ${CURRENT_INSTALLED_DIR}/lib/python3.X/site-packages to PYTHONPATH
 ##
 ## ## Usage
 ## ```cmake
@@ -11,21 +11,12 @@ function(vcpkg_amend_python_path)
   if(DEFINED VCPKG_DOWNLOAD_MODE)
     return()
   endif()
-  if(WIN32)
-    set(PYTHON3 python3)
-  else()
-    find_program(PYTHON3 NAMES python3 python)
-    if(NOT PYTHON3)
-      vcpkg_fail_port_install(
-        ALWAYS
-        MESSAGE "You must have python3 or python installed on this machine to install this port"
-      )
-    endif()
-  endif()
+  set(PYTHON3 python3)
   execute_process(
     COMMAND ${PYTHON3} -c "import sys; print(\"python{}.{}\".format(sys.version_info.major, sys.version_info.minor));"
     OUTPUT_VARIABLE PYTHON3_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
   set(ENV{PYTHONPATH} "${CURRENT_INSTALLED_DIR}/lib/${PYTHON3_VERSION}/site-packages${VCPKG_HOST_PATH_SEPARATOR}$ENV{PYTHONPATH}")
+  message("PYTHONPATH is now $ENV{PYTHONPATH}")
 endfunction()
--- scripts/cmake/vcpkg_pip_install_requirements.cmake
+++ scripts/cmake/vcpkg_pip_install_requirements.cmake
@@ -14,17 +14,7 @@ function(vcpkg_pip_install_requirements)
   if(NOT EXISTS "${CURRENT_PORT_DIR}/requirements.txt")
     return()
   endif()
-  if(WIN32)
-    set(PIP3 pip3)
-  else()
-    find_program(PIP3 NAMES pip3 pip pip3.exe pip.exe)
-    if(NOT PIP3)
-      vcpkg_fail_port_install(
-          ALWAYS
-          MESSAGE "You must have pip3 or pip installed on this machine to install this port"
-      )
-    endif()
-  endif()
+  set(PIP3 pip3)
   vcpkg_execute_required_process(
       COMMAND ${PIP3} install --user -r requirements.txt
       WORKING_DIRECTORY ${CURRENT_PORT_DIR}
